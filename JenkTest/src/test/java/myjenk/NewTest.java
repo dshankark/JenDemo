13191ebb2a979a9318ebad94970c7479c915066d
dshankark <dshankar_k@yahoo.com>
2018-01-13 13:36:55 +0530

    New Repo

---
+++ JenkTest/src/test/java/myjenk/NewTest.java
@@ -0,0 +1,22 @@
+package myjenk;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("Test");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("Before Test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println(" After Test");
+  }
+
+}
